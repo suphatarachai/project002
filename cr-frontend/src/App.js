671a6495a98a703fd2548d99e803ddd0fd79c0e2
suphatarachai <suphatarachai1233@gmail.com>
2022-11-22 22:32:49 +0700

    usestate useeffect

--- cr-frontend/src/App.js
+++ cr-frontend/src/App.js
@@ -1,32 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 
+const App = () => {
+  const [message, setMessage] = useState ('My message 1');
 
-type AppMyState = {
-   message: String;
-};
-
-class App extends React.Component<{}, AppState> {
-  state: AppState = {
-    
-    message: 'Default message',
-  };
-
-  componentDidMount() { 
+  useEffect (() => {
     fetch('http://localhost:3001/courses')
-      .then(res => res.json())
-      .then(obj => {
-        this.setState({message: obj.message});
-      });
-    }
+        .then(res => res.json())
+        .then(obj => {
+          setMessage(obj.message);
+        });
+  },[]);
 
-  render() {
-    return (
-      <div>
-        {this.state.message}
-      </div>
-    );
-  }
+  console.log(message);
+  
+  return (
+    <div className="App">
+       {message}
+    </div>
+
+  );
+  
 }
+
+
+
+// type AppMyState = {
+//    message: String;
+// };
+
+// class App extends React.Component<{}, AppState> {
+//   state: AppState = {
+    
+//     message: 'Default message',
+//   };
+
+//   componentDidMount() { 
+//     fetch('http://localhost:3001/courses')
+//       .then(res => res.json())
+//       .then(obj => {
+//         this.setState({message: obj.message});
+//       });
+//     }
+
+//   render() {
+//     return (
+//       <div>
+//         {this.state.message}
+//       </div>
+//     );
+//   }
+// }
 export default App;
